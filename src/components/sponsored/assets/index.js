8a45937ea68af37ce79c09b58537b2aa28f571f8
boltx123 <udaygarg8188@gmail.com>
2024-05-08 23:01:46 +0530

    .

--- src/components/sponsored/assets/index.js
+++ src/components/sponsored/assets/index.js
@@ -1,9 +1,9 @@
 import sp1 from "./Sde.png";
 import sp2 from "./IMG-20240427-WA0019.jpg";
-import sp3 from "./Fresources.png";
+import sp3 from "./CXI white 18ppi.png";
 import sp4 from "./C.png";
 import sp5 from "./cybercell-removebg-preview.png";
-import sp6 from "./CXI white 18ppi.png";
+import sp6 from "./Fresources.png";
 import sp7 from "./AllIPU.png";
 import sp8 from "./FET.png";
 import sp9 from "./IMG-20240429-WA0003.jpg";
